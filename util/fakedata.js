06b32e824de1203c0ff739cac5d77c68253e34db
cleybson cardoso <cleybsonc@gmail.com>
2018-05-19 00:23:48 -0300

    dashboard

--- util/fakedata.js
+++ util/fakedata.js
@@ -11,27 +11,36 @@ const config = {
 firebase.initializeApp(config);
 
 const db = firebase.firestore();
-db.settings({timestampsInSnapshots: true});
+db.settings({ timestampsInSnapshots: true });
 
-db.collection("itineraries").doc("FYDphmrEU2KQIQz0sbpv").collection("stops").add({
-  location: new firebase.firestore.GeoPoint(-12.8947294, -38.6881958),
-  categories: ["Sol e Mar", "Praia"],
-  description: "Itaparica é um município do estado da Bahia, no Brasil. Fica localizado na Ilha de Itaparica, na Baía de Todos os Santos. Itaparica teve sua fama inicial como balneário de repouso e de saúde devido às suas bonitas praias e à sua água mineral que jorra da Fonte da Bica, localizada dentro da cidade, na costa oeste. Na parede frontal da fonte, está inscrito, em um azulejo: \"Êh ! água fina. Faz velha virá menina\".",
-  duration_in_seconds: 60*60,
-  name: "Itaparica",
-  pictures_url: ["https://media-server.clubmed.com/image/jpeg/1200/675/crop/center/60/https%3A%2F%2Fns.clubmed.com%2Ficp%2F1-MEDIA%2F01.VILLAGES%2F1.1MER%2FITAPARICA%2F9-8-7-6-5-4-3-2-1-PHOTOS%2FITACD112132.jpg"],
+db.collection("itineraries").doc("6PHnWkx8pHXNTZAwcMyF").collection("stops").add({
+  location: new firebase.firestore.GeoPoint(-13.1188168, -38.7970917),
+  categories: ["Nautico", "Praia"],
+  description: "Praia de boa extensão, é uma das mais urbanas da Ilha de Itaparica. Costuma receber um grande número de turistas durante a alta temporada, que aproveitam um belo dia na praia e se divertem à noite nos bares e restaurantes próximos. Seu clima agradável faz dessa praia uma ótima opção para todos os públicos, principalmente para os que gostam de agito. Com uma boa faixa de areia, seu mar é calmo, mais parecendo uma lagoa, muito convidativo para um mergulho. É um lugar movimentado, principalmente à noite, não sendo muito indicado para os que buscam paz e tranquilidade.",
+  duration_in_seconds: 3*60*60,
+  name: "Cacha Pregos",
+  pictures_url: ["https://media-cdn.tripadvisor.com/media/photo-o/05/48/6e/c3/praia-cacha-pregos.jpg"],
   main_picture: "http://www.toursbahia.com.br/loja/wp-content/uploads/2017/09/PASSEIO_-ilha_dos_frades_itaparica_toursbahia_capa-600x550.jpg"
-
 })
 .then(docRef => {
   console.log(docRef.id);
 })
 .catch(err => console.log(err));
 
+// db.collection("itineraries").add({
+//   name: "Circuito Náutico de Cacha-Pregos",
+//   description: "O roteiro inclui dunas, águas transparentes e esportes radicais.",
+//   categories: ["Náutico"]
+// // })
+//   .then(docRef => {
+//     console.log(docRef.id);
+//   })
+//   .catch(err => console.log(err));
+
 db.collection("itineraries").get()
-.then(snaps => {
-  snaps.forEach(snap => {
-    console.log(JSON.stringify(snap.data()));
-  });
-})
-.catch(err => console.log(err));
+  .then(snaps => {
+    snaps.forEach(snap => {
+      console.log(JSON.stringify(snap.data()));
+    });
+  })
+  .catch(err => console.log(err));
